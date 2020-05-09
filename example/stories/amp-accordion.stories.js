ae8bc19e9f162b56400bd4a34ec0f0afd05a084c
prototypa <prototypearea.dev@gmail.com>
2020-05-09 03:25:56 +0200

    Update attribute in examples

--- example/stories/amp-accordion.stories.js
+++ example/stories/amp-accordion.stories.js
@@ -4,7 +4,7 @@ import {storiesOf} from '@storybook/react';
 
 storiesOf('Components/amp-accordion', module).add('default', () => (
   <amp-accordion>
-    <section expanded>
+    <section expanded="">
       <h4>Section 1</h4>
       <p>Bunch of awesome content.</p>
     </section>
