4038233e2fa9fb347e1bbeef4671a7a0f65ea01c
PrototypeArea <prototypearea.dev@gmail.com>
2022-05-12 20:16:37 -0400

    Fix AmpPanel styles for dark mode

--- src/components/AmpPanel.js
+++ src/components/AmpPanel.js
@@ -49,16 +49,15 @@ const AmpPanel = ({ api, channel }) => {
       <div
         className="storybook-amp-source-code"
         style={{
-          backgroundColor: "#f6f8fa",
-          borderLeft: "1px solid #e5e5e5",
           marginTop: 15,
         }}
       >
         <SyntaxHighlighter
           language="html"
           showLineNumbers={false}
-          format={true}
-          copyable={true}
+          format
+          copyable
+          bordered
           padded
         >
           {sourceCode}
@@ -67,79 +66,5 @@ const AmpPanel = ({ api, channel }) => {
     </div>
   );
 };
-
-// class AmpPanel extends React.Component {
-//   state = {
-//     sourceCode: "",
-//   };
-
-//   componentDidMount() {
-//     const { api, channel } = this.props;
-
-//     api.on(STORY_CHANGED, this.onStoryChange);
-//     channel.on(EVENTS.AMP_HTML_RESULT, this.refreshData);
-//   }
-
-//   componentWillUnmount() {
-//     const { api, channel } = this.props;
-
-//     api.off(STORY_CHANGED, this.onStoryChange);
-//     channel.removeListener(EVENTS.AMP_HTML_RESULT, this.refreshData);
-//   }
-
-//   refreshData = (data) => {
-//     this.setState({
-//       sourceCode: data && data.ampHtml ? data.ampHtml : "",
-//     });
-//   };
-
-//   onStoryChange = (id) => {
-//     this.setState({ sourceCode: "" });
-//   };
-
-//   render() {
-//     // #htmlFormat=AMP4ADS
-//     // #htmlFormat=AMP4EMAIL
-//     // #htmlFormat=ACTIONS
-
-//     const { sourceCode } = this.state;
-
-//     return (
-//       <div style={{ padding: 15 }}>
-//         {sourceCode && (
-//           <Button
-//             small
-//             outline
-//             isLink
-//             target="_blank"
-//             href={`https://validator.ampproject.org/#doc=${getBase64ForAMPValidator(
-//               sourceCode
-//             )}`}
-//           >
-//             Validate
-//           </Button>
-//         )}
-//         <div
-//           className="storybook-amp-source-code"
-//           style={{
-//             backgroundColor: "#f6f8fa",
-//             borderLeft: "1px solid #e5e5e5",
-//             marginTop: 15,
-//           }}
-//         >
-//           <SyntaxHighlighter
-//             language="html"
-//             showLineNumbers={false}
-//             format={true}
-//             copyable={true}
-//             padded
-//           >
-//             {sourceCode}
-//           </SyntaxHighlighter>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default AmpPanel;
