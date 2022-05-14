87d8f27619d2649a87091c64a3a2ae251dbdae27
PrototypeArea <prototypearea.dev@gmail.com>
2022-05-14 06:09:12 -0400

    Improve addon panel

--- src/components/AmpPanel.js
+++ src/components/AmpPanel.js
@@ -1,21 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { Button, SyntaxHighlighter } from "@storybook/components";
+import styled from '@emotion/styled';
+import {
+  Button,
+  SyntaxHighlighter,
+  Tabs,
+  WithTooltip,
+  TooltipMessage,
+  Icons
+} from "@storybook/components";
 
 import { STORY_CHANGED } from "@storybook/core-events";
 import EVENTS from "../constants";
 
 import getBase64ForAMPValidator from "../utils/getBase64ForAMPValidator";
+import getHtmlFormatForType from "../utils/getHtmlFormatForType"
+import getAmpLabelForType from "../utils/getAmpLabelForType"
+
+const ValidateButton = styled(Button)(({ theme }) => ({
+  background: theme.barBg,
+}));
+
+const Tooltip = ({ onHide }) => (
+  <TooltipMessage
+    title="Use Online Validator"
+    desc="Validate story using online Amp Validator: https://validator.ampproject.org/"
+  />
+);
 
 const AmpPanel = ({ api, channel }) => {
-  const [sourceCode, setSourceCode] = useState("");
+  const [sourceCode, setSourceCode] = useState(null);
+  const [type, setType] = useState(null);
+
+  const handleValidate = () => {
+    const htmlFormat = getHtmlFormatForType(type);
+    window.open(
+      `https://validator.ampproject.org/#doc=${getBase64ForAMPValidator(
+        sourceCode
+      )}${htmlFormat ? `&htmlFormat=${htmlFormat}` : ''}`,
+      "_newtab" + Date.now()
+    );
+  };
 
   useEffect(() => {
     function handleRefreshData(data) {
+      setType(data && data.type ? data.type : null);
       setSourceCode(data && data.ampHtml ? data.ampHtml : "");
     }
 
     function handleStoryChange(id) {
-      setSourceCode({ sourceCode: "" });
+      setType(null);
+      setSourceCode(null);
     }
 
     api.on(STORY_CHANGED, handleStoryChange);
@@ -27,43 +61,53 @@ const AmpPanel = ({ api, channel }) => {
     };
   }, []);
 
-  // #htmlFormat=AMP4ADS
-  // #htmlFormat=AMP4EMAIL
-  // #htmlFormat=ACTIONS
-
   return (
-    <div style={{ padding: 15 }}>
-      {sourceCode && (
-        <Button
-          small
-          outline
-          isLink
-          target="_blank"
-          href={`https://validator.ampproject.org/#doc=${getBase64ForAMPValidator(
-            sourceCode
-          )}`}
-        >
-          Validate
-        </Button>
-      )}
-      <div
-        className="storybook-amp-source-code"
-        style={{
-          marginTop: 15,
-        }}
-      >
-        <SyntaxHighlighter
-          language="html"
-          showLineNumbers={false}
-          format
-          copyable
-          bordered
-          padded
-        >
-          {sourceCode}
-        </SyntaxHighlighter>
+    <Tabs
+      id="storybook-amp-panel-options"
+      backgroundColor="rgba(0, 0, 0, 0.05)"
+      selected="tabHtmlCode"
+      absolute={true}
+      tools={
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {type ? <div style={{ marginRight: 10}}>{getAmpLabelForType(type)}</div> : null}
+          {sourceCode && (
+            <WithTooltip placement="auto" trigger="hover" tooltip={<Tooltip />}>
+              <ValidateButton
+                small
+                outline
+                type="button"
+                onClick={(e) => handleValidate(e)}
+              >
+                <Icons icon="lightning" />Validate
+              </ValidateButton>
+            </WithTooltip>
+          )}
+        </div>
+      }
+    >
+      <div id="tabHtmlCode" title={"Output"} color="#888">
+       {/* #ff4400 #66bf3c */}
+        <div className="storybook-amp-source-code">
+          {sourceCode === null ? (
+            <div style={{ padding: 10 }}>Loading ...</div>
+          ) : (
+            <SyntaxHighlighter
+              language="html"
+              showLineNumbers
+              format
+              copyable
+              bordered={false}
+              padded
+            >
+              {sourceCode}
+            </SyntaxHighlighter>
+          )}
+        </div>
       </div>
-    </div>
+      {/* <div id="tabDetails" title={"Details"}>
+        Details
+      </div> */}
+    </Tabs>
   );
 };
 
